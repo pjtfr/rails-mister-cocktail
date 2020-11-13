9240ac495b9bb59945518bc7d52418d6008b8786
Pierre Jean Testa <piertesta75@gmail.com>
2020-11-13 15:05:01 +0100

    rails-mister-cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,3 +30,18 @@ class CocktailsController < ApplicationController
     params.require(:cocktail).permit(:name)
   end
 end
+
+# Create
+get    'songs/new',      to: 'songs#new'
+post   'songs',          to: 'songs#create'
+
+# Read
+get    'songs',          to: 'songs#index'
+get    'songs/:id'       to: 'songs#show', as: 'song'
+
+# Update
+get    'songs/:id/edit', to: 'songs#edit'
+patch  'songs/:id',      to: 'songs#update'
+
+# Destroy
+delete 'songs/:id',      to: 'songs#destroy'
